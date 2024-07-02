3a8b27fbd8bd9fe3123dd03cb31888b488c1314a
qingqinzhan <qingqinzhan@tencent.com>
2024-07-02 20:57:04 +0800

    feat: change tokyonight color

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,5 +1,11 @@
 return {
   "folke/tokyonight.nvim",
   lazy = true,
-  opts = { style = "night" },
+  opts = {
+    style = "night",
+    ---@param colors ColorScheme
+    on_colors = function(colors)
+      colors.bg_highlight = "#444a73"
+    end,
+  },
 }
