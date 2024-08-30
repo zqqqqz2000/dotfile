6d544f444b7cf4e15cac8aaeaddd117fc40b31c0
qingqinzhan <qingqinzhan@tencent.com>
2024-08-30 22:15:58 +0800

    chore: disable activate firenvim on iwiki

--- .config/nvim/lua/plugins/firenvim.lua
+++ .config/nvim/lua/plugins/firenvim.lua
@@ -1,8 +1,22 @@
+vim.g.firenvim_config = {
+  globalSettings = { alt = "all" },
+  localSettings = {
+    [".*iwiki.*"] = {
+      priority = 1,
+      takeover = "never",
+    },
+  },
+}
+
 if vim.g.started_by_firenvim == true then
   vim.o.cmdheight = 0
   vim.o.statusline = ""
   vim.o.laststatus = 0
+
+  vim.api.nvim_set_keymap("n", "<Esc><Esc>", "<Cmd>wq<CR><Cmd>call firenvim#focus_page()<CR>", {})
+
   return {
+    { "glacambre/firenvim", build = ":call firenvim#install(0)" },
     {
       "nvim-lualine/lualine.nvim",
       enabled = false,
@@ -17,7 +31,6 @@ if vim.g.started_by_firenvim == true then
       },
     },
     { "rcarriga/nvim-notify", enabled = false },
-    { "glacambre/firenvim", build = ":call firenvim#install(0)" },
   }
 else
   return { "glacambre/firenvim", build = ":call firenvim#install(0)" }
