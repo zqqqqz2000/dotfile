1b76b049daac689075558eae1ee31968110c76bc
zqqqqz2000 <zqqqqz2000@sina.cn>
2024-08-30 22:56:03 +0800

    refactor: merge neopyter to python

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -2,6 +2,12 @@ return {
   {
     "linux-cultist/venv-selector.nvim",
     cmd = "VenvSelect",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-telescope/telescope.nvim",
+      "mfussenegger/nvim-dap-python",
+      "nvim-dap",
+    },
     opts = function(_, opts)
       if require("lazyvim.util").has("nvim-dap-python") then
         opts.dap_enabled = true
@@ -24,6 +30,31 @@ return {
         black = {
           prepend_args = { "--line-length", "120" },
         },
+      },
+    },
+  },
+  {
+    "SUSTech-data/neopyter",
+    dependencies = {
+      "AbaoFromCUG/websocket.nvim",
+    },
+    ---@type neopyter.Option
+    opts = {
+      mode = "direct",
+      remote_address = "127.0.0.1:9001",
+      file_pattern = { "*.ju.*" },
+      on_attach = function(buf)
+        local function map(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { desc = desc, buffer = buf })
+        end
+        map("n", "<leader><CR>", "<cmd>Neopyter run current<cr>", "run selected")
+        map("n", "<leader>n", "/# %%<cr>", "next cell")
+        map("n", "<leader>r", "<cmd>Neopyter kernel restart<cr>", "restart kernel")
+        map("n", "<leader>rr", "<cmd>Neopyter execute notebook:restart-run-all<cr>", "restart kernel and run all")
+      end,
+      highlight = {
+        enable = true,
+        shortsighted = false,
       },
     },
   },
