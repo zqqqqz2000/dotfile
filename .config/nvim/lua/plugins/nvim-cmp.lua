e019cf12a1e1b8108ec2c077ef63f7ec16bc9616
zqqqqz2000 <zqqqqz2000@sina.cn>
2024-09-19 23:11:24 +0800

    fix: nvim copilot config in cmp

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -7,6 +7,20 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
+    {
+      "zbirenbaum/copilot-cmp",
+      dependencies = "copilot.lua",
+      opts = {},
+      config = function(_, opts)
+        local copilot_cmp = require("copilot_cmp")
+        copilot_cmp.setup(opts)
+        -- attach cmp source whenever copilot attaches
+        -- fixes lazy-loading issues with the copilot cmp source
+        LazyVim.lsp.on_attach(function(client)
+          copilot_cmp._on_insert_enter({})
+        end, "copilot")
+      end,
+    },
   },
   opts = function()
     vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
@@ -38,11 +52,15 @@ return {
           require("luasnip").lsp_expand(args.body)
         end,
       },
-
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
         { name = "path" },
+        {
+          name = "copilot",
+          group_index = 1,
+          priority = 100,
+        },
       }, {
         { name = "buffer" },
       }),
