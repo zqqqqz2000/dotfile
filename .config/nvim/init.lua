387089a01921655854dea99978f31d4e8a218109
zqqqqz2000 <zqqqqz200@sina.cn>
2024-11-18 21:11:06 +0800

    feat: neovim fouce osc52 copy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.wo.cursorcolumn = true
-vim.opt.termguicolors = true
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,28 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local function paste()
+  return {
+    vim.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
+
+vim.opt.clipboard = { "unnamedplus" }
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.cursorcolumn = true
+vim.opt.termguicolors = true
