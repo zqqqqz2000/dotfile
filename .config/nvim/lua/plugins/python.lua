7f15b8a8b11dc3021d8a053e449d5e3e4d836890
qingqinzhan <qingqinzhan@tencent.com>
2025-01-16 17:04:37 +0800

    fix: remove flake for some error

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -26,6 +26,9 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters_by_ft = {
+        ["python"] = { "black", "isort" },
+      },
       formatters = {
         black = {
           prepend_args = { "--line-length", "120" },
