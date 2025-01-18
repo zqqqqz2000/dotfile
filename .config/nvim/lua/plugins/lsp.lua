a36ab881208a8655674d6c7720cb2206e10bce2a
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-01-18 12:33:36 +0800

    fix: disable clangd parse .proto file

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
+  opts = function(_, opts)
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = {
       "<c-k>",
       false,
       mode = "i",
     }
+    opts.servers.clangd.filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }
   end,
 }
