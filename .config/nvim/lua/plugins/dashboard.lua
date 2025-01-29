d10f3241b76cccda9ff78f2178c3e07ac7b4f3cb
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-01-30 03:14:08 +0800

    feat: vim startup page support session search

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,22 +1,36 @@
 return {
-  "snacks.nvim",
-  opts = {
-    dashboard = {
-      sections = {
-        {
-          section = "terminal",
-          cmd = "pokemon-colorscripts -r --no-title; sleep .1",
-          random = 10,
-          indent = 15,
-          height = 20,
-        },
-        {
-          pane = 2,
-          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-          { section = "startup" },
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        sections = {
+          {
+            section = "terminal",
+            cmd = "pokemon-colorscripts -r --no-title; sleep .1",
+            random = 10,
+            indent = 15,
+            height = 20,
+          },
+          {
+            pane = 2,
+            { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+            { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+            { section = "startup" },
+          },
         },
       },
     },
+  },
+  {
+    "folke/snacks.nvim",
+    optional = true,
+    opts = function(_, opts)
+      table.insert(opts.dashboard.preset.keys, 1, {
+        action = "<leader>qS",
+        desc = "Sessions",
+        icon = "➤ ",
+        key = "S",
+      })
+    end,
   },
 }
