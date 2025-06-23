2c5b150d3764c82bb6518645a20900189d3f1686
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-06-23 15:31:01 +0800

    fix: blink workaround for newest lazyvim

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,9 +1,15 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    completion = {},
     keymap = {
-      preset = "super-tab",
+      -- preset = "super-tab",
+      -- temporary fix for supertab
+      -- https://github.com/LazyVim/LazyVim/issues/6185
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
       ["<C-u>"] = { "scroll_documentation_up", "fallback" },
